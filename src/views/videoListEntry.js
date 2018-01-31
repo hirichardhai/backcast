2fb950e7e3b36860af351b56a6974326b805f66b
Richard Hai <hirichardhai@gmail.com>
2018-01-30 16:49:10 -0800

    finished up to videos

--- src/views/videoListEntry.js
+++ src/views/videoListEntry.js
@@ -2,6 +2,7 @@ var VideoListEntryView = Backbone.View.extend({
 
 
   render: function() {
+  	console.log(this.el);
     this.$el.html(this.template(this.model.attributes));
     return this;
   },
